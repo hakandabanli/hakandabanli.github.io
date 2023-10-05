c15db7e953e830be5a481eae5db81d06a1ac369e
Hakan <hakan.dabanli@gameontech.co.uk>
2023-10-05 18:39:24 +0300

    Updates

--- js/main.js
+++ js/main.js
@@ -4,3 +4,9 @@ function GoHomepage(){
 function GoContactForm(){
     window.location.href = '/contact-us.html';
 }
+function GoSarenteGamesContactForm(){
+    window.location.href = '/urunler/sarentegames/contact.html';
+}
+function GoAllSarenteGames(){
+    window.location.href = '/urunler/sarentegames/games.html';
+}
